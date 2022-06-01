e0f70bed4c906c2141a751ff337df73e88517b29
zh0rchik <99082375+zh0rchik@users.noreply.github.com>
2022-06-01 23:15:13 +0400

    Add files via upload

--- scripts/script5.php
+++ scripts/script5.php
@@ -6,14 +6,23 @@
     $array = []; //Матрица
 
     for($i = 0; $i < count($row_matrix);$i++){
-        $column_row_matrix = explode(' ', $row_matrix[$i]);
+        $column_row_matrix = explode(' ', trim($row_matrix[$i]));
         $array_row = [];
 
         for($j = 0; $j < count($column_row_matrix); $j++){
-                array_push($array_row, $column_row_matrix[$j]);
+            if(count($column_row_matrix) == 1 && $column_row_matrix[$j] == '')
+                die("Ошибка: лишние переносы!");
+            array_push($array_row, $column_row_matrix[$j]);
         }
-
         array_push($array, $array_row);
+    }
+
+    for($i = 0; $i < count($array); $i++){
+        for($j = 0; $j < count($array[$i]); $j++){
+            if(count($array) != count($array[$i])){
+                die("Ошибка: матрица должна быть квадратной!");
+            }
+        }
     }
 
     $n = count($column_row_matrix);
@@ -23,8 +32,10 @@
             for ($j = 0; $j < count($column_row_matrix); $j++)
                 $array[$i][$j] = ($array[$i][$j] || ($array[$i][$k] && $array[$k][$j]));
 
+    for ($i = 0; $i < count($column_row_matrix); $i++)
+        $array[$i][$i] = 1;
+
     for ($i = 0; $i < count($column_row_matrix); $i++) {
-        echo "<br>";
         for ($j = 0; $j < count($column_row_matrix); $j++) {
             if ($array[$i][$j])
                 echo "1";
@@ -32,6 +43,7 @@
                 echo "0";
             echo " ";
         }
+        echo "<br>";
     }
 
 ?>
